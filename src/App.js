085040867fe5895a640ab8f548984c17f11c1f50
unknown <narjessheikhali@gmail.com>
2022-05-07 14:26:12 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Usertable from './components/UserTable/Usertable';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <Usertable/>
+    </div>
+  );
+}
+
+export default App;

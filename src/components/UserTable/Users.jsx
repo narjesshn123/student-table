a33098ce7297d1628e2795dbdbcf880a0484e23c
unknown <narjessheikhali@gmail.com>
2022-05-11 14:29:36 +0430

    first commit

--- src/components/UserTable/Users.jsx
+++ src/components/UserTable/Users.jsx
@@ -1,2 +1,13 @@
-
-  export default users;          
+const Users = [
+  {id:1, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:2, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:3, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:4, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:5, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:6, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:7, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:8, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:9, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+  {id:10, name: 'narjes', lastName: 'sheikhali', career: "programmer" },
+]
+  export default Users;          
--- src/components/UserTable/Usertable.jsx
+++ src/components/UserTable/Usertable.jsx
@@ -1,75 +1,96 @@
 import  './Usertable.css';
 import { Component } from "react"         
+import Users from './Users';
 
-const users = [
-    {id: 1, name: "narjes", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 2, name: "zahra", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 3, name: "mojde", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 4, name: "hadi", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 5, name: "amin", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 6, name: "mami", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 7, name: "dadi", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 8, name: "mahi", status: "developer", email: "narjessheikhali@gmail.com"},
-    {id: 9, name: "shahi", status: "developer", email: "narjessheikhali@gmail.com"}, 
-  ]
-
-  
-  class Usertable extends Component{
-      state = {
-          users:users
-      }
-      handleDeleteUser = (id)=>{
-          this.setState({users : this.state.users.filter(user=>
-            user.id!==id)})
-      }
-      handleAddUser = () => {
-          const name = prompt("enter user name")
-          const status = prompt("enter user status")
-          const email = prompt("enter user email")
-          this.setState({users:[...this.state.users,{id:Math.round(Math.random()*1000), name, status, email}]})
-      }
-      handleUpdateUser = (olduser)=>{
-        const name = prompt("enter user name",olduser.name)
-        const status = prompt("enter user status", olduser.status)
-        const email = prompt("enter user email", olduser.email) 
-        this.setState({users: this.state.users.map(user=>
-        user.id===olduser.id ?
-        {...olduser, name, status, email}
-        :user)})
-      }
-      render(){
-          return(
-              <div>
-              <table className='customers'>
-                  <thead>
-                      <tr>
-                          <td>id</td>
-                          <td>name</td>
-                          <td>status</td>
-                          <td>email</td>
-                          <td>
-                  <button onClick={this.handleAddUser}>Add</button>
-                          </td>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      {this.state.users.map(user => (
-                          <tr key={user.id}>
-                              <td>{user.id}</td>
-                              <td>{user.name}</td>
-                              <td>{user.status}</td>
-                              <td>{user.email}</td>
-                              <td>
-                                  <button onClick={()=>this.handleUpdateUser(user)}>update</button>
-                                  <button onClick={()=>this.handleDeleteUser(user.id)}>{"delete"}</button>
-                              </td>
-                          </tr>
-                      ))}
-                  </tbody>
-              </table>
-              </div>
-          )
-      }
+class Usertable extends Component{
+  state={
+      Users:Users
   }
+    render(){
+        return(
+        <>
+        <table>
+            <thead>
+            <tr>
+                <td>id</td>
+                <td>name</td>
+                <td>lastName</td>
+                <td>career</td>
+            </tr>
+            </thead>
+            <tbody>
+                {Users.map((user, index)=>(
+                    <tr key={index}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.career}</td>
+                    </tr>
+                    
+               ))}
+                
+            </tbody>
+        </table>
+        </>)
+    }
+}
+  
+//   class Usertable extends Component{
+//       state = {
+//           users:users
+//       }
+//       handleDeleteUser = (id)=>{
+//           this.setState({users : this.state.users.filter(user=>
+//             user.id!==id)})
+//       }
+//       handleAddUser = () => {
+//           const name = prompt("enter user name")
+//           const status = prompt("enter user status")
+//           const email = prompt("enter user email")
+//           this.setState({users:[...this.state.users,{id:Math.round(Math.random()*1000), name, status, email}]})
+//       }
+//       handleUpdateUser = (olduser)=>{
+//         const name = prompt("enter user name",olduser.name)
+//         const status = prompt("enter user status", olduser.status)
+//         const email = prompt("enter user email", olduser.email) 
+//         this.setState({users: this.state.users.map(user=>
+//         user.id===olduser.id ?
+//         {...olduser, name, status, email}
+//         :user)})
+//       }
+//       render(){
+//           return(
+//               <div>
+//               <table className='customers'>
+//                   <thead>
+//                       <tr>
+//                           <td>id</td>
+//                           <td>name</td>
+//                           <td>status</td>
+//                           <td>email</td>
+//                           <td>
+//                   <button onClick={this.handleAddUser}>Add</button>
+//                           </td>
+//                       </tr>
+//                   </thead>
+//                   <tbody>
+//                       {this.state.users.map(user => (
+//                           <tr key={user.id}>
+//                               <td>{user.id}</td>
+//                               <td>{user.name}</td>
+//                               <td>{user.status}</td>
+//                               <td>{user.email}</td>
+//                               <td>
+//                                   <button onClick={()=>this.handleUpdateUser(user)}>update</button>
+//                                   <button onClick={()=>this.handleDeleteUser(user.id)}>{"delete"}</button>
+//                               </td>
+//                           </tr>
+//                       ))}
+//                   </tbody>
+//               </table>
+//               </div>
+//           )
+//       }
+//   }
 
 export default Usertable;       
